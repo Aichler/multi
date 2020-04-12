3aaf53081b987fe1099788072f2b69f84ce733aa
Aichler <alexey987@bk.ru>
2020-04-12 23:12:17 +0600

    'Working'

--- src/main/java/project/main/Test.java
+++ src/main/java/project/main/Test.java
@@ -13,6 +13,10 @@ public class Test {
 
     public static void main(String args[]) throws InterruptedException {
         final Thiev thiev = new Thiev();
+//        final Thiev thiev1 = new Thiev();
+//        final Thiev thiev2 = new Thiev();
+//        final Thiev thiev3 = new Thiev();
+
         Thread threadAdd = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -60,33 +64,48 @@ public class Test {
         });
 
 
-        thiev.people.add("Tomc");
-        thiev.people.add("Alidfe");
-        thiev.people.add("Katf");
-        thiev.people.add("Sanv");
-        thiev.people.add("Tuym");
-        thiev.people.add("Axice");
-        thiev.people.add("Knte");
-        thiev.people.add("Srm");
-        thiev.people.add("Thm");
+        thiev.people.add("Tomcs");
+        thiev.people.add("Tomcd");
+        thiev.people.add("Tomcf");
+
+        thiev.people.add("Sanvc");
+        thiev.people.add("Sanvf");
+        thiev.people.add("Sanvb");
+
+
+        thiev.people.add("Thmz");
+        thiev.people.add("Thmx");
+        thiev.people.add("Thmb");
+
         thiev.people.add("Avice");
-        thiev.people.add("Kfte");
-        thiev.people.add("Sdm");
+
 
 
         threadAdd.start();
         threadAdd1.start();
-        threadAdd2.start();
+
         System.out.println(threadAdd.isAlive());
         System.out.println(threadRemove.isAlive());
         System.out.println("========================");
         // получаем 2-й объект
 //        threadRemove.sleep();
+        threadRemove.sleep(5000);
         threadRemove.start();
         System.out.println(threadAdd.isAlive());
         System.out.println(threadRemove.isAlive());
 //        threadRemove.sleep(3000);
+        threadAdd2.start();
 
+        System.out.println("++++");
+        System.out.println(threadAdd2.isAlive());
+        System.out.println(threadRemove.isAlive());
+
+
+        threadAdd.join();
+        threadAdd1.join();
+
+        threadRemove.join();
+        threadAdd2.join();
     }
 
 
@@ -94,6 +113,7 @@ public class Test {
 }
 
 class Thiev{
+
      volatile CopyOnWriteArrayList<Object> hous = new CopyOnWriteArrayList<Object>();
      CopyOnWriteArrayList<Object> people = new CopyOnWriteArrayList<Object>();
 
@@ -105,15 +125,17 @@ class Thiev{
         synchronized (Thiev.class) {
 
 
-            for (int i = 0; i < 4; i++) {
-                int t = random.nextInt(12);
+//            for (int i = 0; i < 4; i++) {
+                int t = random.nextInt(3);
+//                int t = 0;
                 System.out.println("Это начал работать " + t + " поток (хозяин)");//
                 hous.add(people.get(t));
                 System.out.println(hous);
 //            wait();
+//                notify();
 //            System.out.println(hous);//
                 System.out.println("Это отработал " + t + " поток");//
-            }
+//            }
         }
     }
 
@@ -121,16 +143,35 @@ class Thiev{
     {
         synchronized (Thiev.class) {
             Random random = new Random();
-            int i = random.nextInt(10);
+            int i = random.nextInt(3);
+            System.out.println("Lol " + hous.get(i));
             System.out.println("Это начал работать " + i + " поток (вор)");//
-//            notify();
+//
+
+//            wait();
             thef.add(hous.get(i));
+            System.out.println("KEK");//
         hous.remove(i);
             System.out.println("вор " + thef);
         System.out.println("Вор украл: "+ thef + " и в доме осталось : " + hous);//
             System.out.println("Это отработал " + i + " поток");//
+//            wait();
         }
 
     }
 
+
+
 }
+
+//class Thing{
+//    private String name;
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//}
